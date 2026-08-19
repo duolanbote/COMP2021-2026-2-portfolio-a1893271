66d2d632c1e738dd43449a71654b5af608248542
MAX <2638995179lyx@gmail.com>
2026-08-19 12:47:46 +0930

    task3 and task4

---
+++ Week3/PracB.Tests/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿public class EmployeeTests
+{
+    [Fact]
+    public void FullTimeEmployeeCalculatePayTest()
+    {
+        FullTimeEmployee employee = new FullTimeEmployee();
+
+        employee.Name = "Peter";
+        employee.AnnualSalary = 1000m;
+
+        decimal result = employee.CalculatePay();
+
+        Assert.Equal(800m, result);
+    }
+
+    [Fact]
+    public void ContractorCalculatePayTest()
+    {
+        Contractor contractor = new Contractor();
+
+        contractor.Name = "Max";
+        contractor.Rate = 50m;
+        contractor.Hours = 10m;
+
+        decimal result = contractor.CalculatePay();
+
+        Assert.Equal(400m, result);
+    }
+
+    [Fact]
+    public void FullTimeEmployeeReportTest()
+    {
+        FullTimeEmployee employee = new FullTimeEmployee();
+
+        employee.Name = "Jesus";
+        employee.AnnualSalary = 1000m;
+
+        string result = employee.GenerateReport();
+
+        Assert.Contains("Jesus", result);
+    }
+
+    [Fact]
+    public void ContractorReportTest()
+    {
+        Contractor contractor = new Contractor();
+
+        contractor.Name = "Max";
+        contractor.Rate = 50m;
+        contractor.Hours = 10m;
+
+        string result = contractor.GenerateReport();
+
+        Assert.Contains("Max", result);
+    }
+}
