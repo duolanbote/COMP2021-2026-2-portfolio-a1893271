f38bb8ff3bbfc6cb93a73d8746394a9c06a047b7
MAX <2638995179lyx@gmail.com>
2026-09-01 11:04:09 +0930

    task1

---
+++ Week5/Topic5A/Program.cs
@@ -0,0 +1,71 @@
+﻿string[] names = new string[]
+{
+    "Max",
+    "John",
+    "Alice",
+    "Tom",
+    "Michael",
+    "Jack",
+    "Emma",
+    "David",
+    "Alexander",
+    "Sam"
+};
+
+Console.WriteLine("All names:");
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+
+string longest = names[0];
+string shortest = names[0];
+
+foreach (string name in names)
+{
+    if (name.Length > longest.Length)
+    {
+        longest = name;
+    }
+
+    if (name.Length < shortest.Length)
+    {
+        shortest = name;
+    }
+}
+
+Console.WriteLine("Longest name: " + longest);
+Console.WriteLine("Shortest name: " + shortest);
+
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Array.Sort(names);
+
+Console.WriteLine("sorting:");
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Array.Reverse(names);
+
+Console.WriteLine("Reversing:");
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
