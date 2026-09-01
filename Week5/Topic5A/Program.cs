6c9a80fa52eedb70809ad2858e108eab78cde2d1
MAX <2638995179lyx@gmail.com>
2026-09-01 11:52:55 +0930

    task2

--- Week5/Topic5A/Program.cs
+++ Week5/Topic5A/Program.cs
@@ -69,3 +69,85 @@ foreach (string name in names)
 {
     Console.WriteLine(name);
 }
+
+
+List<string> students = new List<string>();
+
+
+//task2
+
+students.Add("Max");
+students.Add("Aya");
+students.Add("Trace");
+students.Add("Tom");
+
+
+students.Insert(1, "Emma");
+
+students.Remove("Tom");
+
+students.AddRange(new string[]
+{"David","Jack","Michael", "Sam", "Alex666", "Prince"});
+
+Console.WriteLine("Student List:");
+
+foreach (string student in students)
+{
+    Console.WriteLine(student);
+}
+
+//search
+string searchName = "Alice";
+
+for (int i = 0; i < students.Count; i++)
+{
+    if (students[i] == searchName)
+    {
+        Console.WriteLine(searchName + " is at index " + i);
+    }
+}
+
+//contain
+string partialName = "a";
+
+Console.WriteLine("Names containing " + partialName + ":");
+
+foreach (string student in students)
+{
+    if (student.Contains(partialName))
+    {
+        Console.WriteLine(student);
+    }
+}
+
+//list length
+int Length = 0;
+
+foreach (string student in students)
+{
+    Length += student.Length;
+}
+
+Console.WriteLine("\nTotal length: " + Length);
+
+
+// Convert
+List<string> oldNames = new List<string>(names);
+
+
+// Add Task 1 names to Task 2 List
+students.AddRange(oldNames);
+
+
+foreach (string student in students)
+{
+    Console.WriteLine(student);
+}
+
+
+
+//uncompleted: task2 output and task3 reflection
+
+//must be finished when i back home
+
+//cao ni ma yi ding yao ji de
