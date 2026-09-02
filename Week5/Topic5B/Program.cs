63e4feeffc08a730015d24222156ba1b12e9ee5f
MAX <2638995179lyx@gmail.com>
2026-09-02 12:05:47 +0930

    save

--- Week5/Topic5B/Program.cs
+++ Week5/Topic5B/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+var numbers = new LinkedList<int>();
+
+numbers.AddFirst("Jody");
+numbers.AddLast("wendys");
+
+
+
+Console.WriteLine(numbers);
