335d67b5262759bed01eef2bd5fe4c183f1989d9
MAX <2638995179lyx@gmail.com>
2026-09-08 11:03:56 +0930

    task1

---
+++ Week6/Topic6A/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<string> history = new Stack<string>();
+
+        history.Push("Text");
+        history.Push("Image");
+        history.Push("Colour");
+
+        Console.WriteLine("Initial Stack:");
+
+        foreach (string action in history)
+        {
+            Console.WriteLine(action);
+        }
+
+        Console.WriteLine("\nTop item:");
+        Console.WriteLine(history.Peek());
+
+        history.Pop();
+
+        Console.WriteLine("Other item Pop:");
+
+        foreach (string action in history)
+        {
+            Console.WriteLine(action);
+        }
+    }
+}
