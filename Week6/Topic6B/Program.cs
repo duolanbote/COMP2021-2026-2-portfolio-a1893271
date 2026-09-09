0c983600b7b2604f4eac3bbc076059ae2e9e830a
MAX <2638995179lyx@gmail.com>
2026-09-09 12:00:56 +0930

    task1

--- Week6/Topic6B/Program.cs
+++ Week6/Topic6B/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> names = new List<string>()
+        {
+            "Tom",
+            "Jack",
+            "Alice",
+            "David",
+            "Bob",
+            "Lucy",
+            "John",
+            "Emma",
+            "Peter",
+            "Mary"
+        };
+
+        var orderedNames = names.OrderBy(x => x);
+
+        List<string> sortedNames = orderedNames.ToList();
+
+        foreach (string name in sortedNames)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
